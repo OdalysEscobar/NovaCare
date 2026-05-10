4a07259838c929e4d27a4492be874966fe5e37a2
ariana2114 <arianamites2114@gmail.com>
2026-05-09 22:56:44 -0500

    Modificación en Launcher

--- src/main/java/ec/edu/uce/novacare/Launcher.java
+++ src/main/java/ec/edu/uce/novacare/Launcher.java
@@ -2,20 +2,20 @@ package ec.edu.uce.novacare;
 
 import ec.edu.uce.novacare.interfaz.MenuIngresarSistema;
 import ec.edu.uce.novacare.interfaz.MenuPrincipal;
-import ec.edu.uce.novacare.interfaz.MenuIngresarSistema;
 
 public class Launcher {
 
     public static void main(String[] args) {
 
+        MenuIngresarSistema menu1 = new MenuIngresarSistema();
+
+        menu1.mostrarMenu();
 
         MenuPrincipal menu = new MenuPrincipal();
 
         menu.mostrarMenu();
 
-        MenuIngresarSistema menu1 = new MenuIngresarSistema();
 
-        menu1.mostrarMenu();
     }
 
 }
