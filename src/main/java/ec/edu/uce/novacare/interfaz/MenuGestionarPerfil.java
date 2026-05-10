16f52a7aded169add90093ce0c77409bfafc8a53
ariana2114 <arianamites2114@gmail.com>
2026-05-09 21:26:06 -0500

    Validacion en Menu principal

---
+++ src/main/java/ec/edu/uce/novacare/interfaz/MenuGestionarPerfil.java
@@ -0,0 +1,64 @@
+package ec.edu.uce.novacare.interfaz;
+
+public class MenuGestionarPerfil {
+
+    public void mostrarMenu() {
+
+        int opcion;
+
+        do {
+            System.out.println("===== MENU: GESTIONAR PERFIL =====");
+            System.out.println("1. Crear perfil de usuario");
+            System.out.println("2. Actualizar perfil de usuario");
+            System.out.println("3. Consultar perfil de usuario");
+            System.out.println("4. Eliminar perfil de usuario");
+            System.out.println("0. Volver al menú principal");
+
+            System.out.println("Seleccione una opción: ");
+
+            while (!scanner.hasNextInt()) {
+                System.out.println("Error: solo puede ingresar números");
+                scanner.next();
+                System.out.print("Seleccione una opcion: ");
+            }
+
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+
+                    System.out.println("Iniciando sesión...");
+                    break;
+
+                case 2:
+
+                    System.out.println("Registrando usuario...");
+                    break;
+
+                case 3:
+                    System.out.println("Registrando usuario...");
+                    break;
+
+                case 4:
+
+                    System.out.println("Registrando usuario...");
+                    break;
+
+                case 0:
+
+                    System.out.println("Regresando al menú principal...");
+                    return;
+
+                default:
+
+                    System.out.println("Opción inválida.");
+                    break;
+            }
+
+        } while (opcion != 0);
+
+
+
+
+    }
+}
