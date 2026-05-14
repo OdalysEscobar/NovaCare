585c62a02c2f012eaf3c9b927dd0509c43bb7620
Nicole Escobar <nicoleescobar98@hotmail.com>
2026-05-13 23:44:28 -0500

    registrar usuario

--- src/main/java/ec/edu/uce/novacare/interfaz/RegistrarUsuario.java
+++ src/main/java/ec/edu/uce/novacare/interfaz/RegistrarUsuario.java
@@ -1,6 +1,10 @@
 package ec.edu.uce.novacare.interfaz;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 
 public class RegistrarUsuario {
     Scanner scanner=new Scanner (System.in);
@@ -8,24 +12,44 @@ public class RegistrarUsuario {
     public RegistrarUsuario () {
     }
 
-    public boolean guardarUsuario (){
+    public void guardarUsuario () {
         System.out.println("=== Registrar Usuario ===");
         String nombre;
         String apellido;
         String correo;
         String contrasena;
+        boolean checkNombre;
+        boolean checkApellido;
 
-        System.out.println("Ingresa tu nombre: ");
-        nombre = scanner.nextLine();
+        do {
+            System.out.println("Ingresa tu nombre: ");
+            nombre = scanner.nextLine();
+            checkNombre = validarLetras(nombre);
+            if (checkNombre == false) {
+                System.out.println("El nombre no cumple con los parámetros. Solo se permiten letras.");
+            }
+        } while (checkNombre==false);
 
-        System.out.println("Ingresa tu apellido: ");
-        apellido = scanner.nextLine();
+        do{
+            System.out.println("Ingresa tu apellido: ");
+            apellido = scanner.nextLine();
+            checkApellido = validarLetras(apellido);
+            if(checkApellido==false){
+                System.out.println("El apellido no cumple con los parametros");
+            }
+        }while(checkApellido==false);
 
         System.out.println("Ingresa tu correo: ");
         correo = scanner.nextLine();
 
         System.out.println("Ingresa tu contraseña: ");
         contrasena = scanner.nextLine();
-        return  true;
+
+    }
+
+    public boolean validarLetras(String info){
+        Pattern pattern = Pattern.compile("^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$");
+        Matcher matcher = pattern.matcher(info);
+        return matcher.matches();
     }
 }
