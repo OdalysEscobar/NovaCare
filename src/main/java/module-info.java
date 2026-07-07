f7e016e9bbcde1533f51046ff034e81cc7101c26
Odalys <ogescobarc@uce.edu.ec>
2026-07-07 00:07:50 -0500

    arreglo del module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,11 +2,12 @@ module ec.edu.uce.novacare {
     requires javafx.controls;
     requires javafx.fxml;
     requires jdk.unsupported.desktop;
-    requires ec.edu.uce.novacare;
 
-
-    opens ec.edu.uce.novacare to javafx.fxml;
     exports ec.edu.uce.novacare;
     exports ec.edu.uce.novacare.util;
+    exports ec.edu.uce.novacare.dominio;
+    exports ec.edu.uce.novacare.DAO;
+
+    opens ec.edu.uce.novacare to javafx.fxml;
     opens ec.edu.uce.novacare.util to javafx.fxml;
 }
